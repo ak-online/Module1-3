b0f6f39ae3329596c49df61b76ccf43a22b2b9be
ak-online <79262567597@yandex.com>
2024-08-09 21:41:53 +0300

    Update module_1_2.py

--- module_1_2.py
+++ module_1_2.py
@@ -1,11 +1,11 @@
 qnty_of_tasks = 12
 qnty_of_hours = 1.5
-name_of_corse = 'Python'
+name_of_course = 'Python'
 time_per_one_task = qnty_of_hours/qnty_of_tasks
 #option1
-print('Курс :',name_of_corse,', всего задач: ',qnty_of_tasks,', затрачено часов:',qnty_of_hours,', среднее время выполнения :',time_per_one_task,' часа.')
+print('Курс :',name_of_course,', всего задач:',qnty_of_tasks,', затрачено часов:',qnty_of_hours,', среднее время выполнения :',time_per_one_task,' часа.')
 
 #option2
-final_string ='Курс :'+name_of_corse+', всего задач: '+str(qnty_of_tasks)+', затрачено часов:'+str(qnty_of_hours)+', среднее время выполнения :'+str(time_per_one_task)+' часа.'
+time_per_one_task = str(qnty_of_hours/qnty_of_tasks)
+final_string ='Курс : '+name_of_course+', всего задач: '+str(qnty_of_tasks)+', затрачено часов: '+str(qnty_of_hours)+', среднее время выполнения : '+time_per_one_task+' часа.'
 print(final_string)
-
