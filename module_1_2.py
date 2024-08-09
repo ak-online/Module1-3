129d0a126e0997c37e9453b7fc7524153a3bea4a
ak-online <79262567597@yandex.com>
2024-08-09 21:38:31 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,11 @@
+qnty_of_tasks = 12
+qnty_of_hours = 1.5
+name_of_corse = 'Python'
+time_per_one_task = qnty_of_hours/qnty_of_tasks
+#option1
+print('Курс :',name_of_corse,', всего задач: ',qnty_of_tasks,', затрачено часов:',qnty_of_hours,', среднее время выполнения :',time_per_one_task,' часа.')
+
+#option2
+final_string ='Курс :'+name_of_corse+', всего задач: '+str(qnty_of_tasks)+', затрачено часов:'+str(qnty_of_hours)+', среднее время выполнения :'+str(time_per_one_task)+' часа.'
+print(final_string)
+
