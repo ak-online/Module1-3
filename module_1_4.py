3c0dcb7c526d1c6d1b7ea40d3e4a931bc577ccaf
ak-online <79262567597@yandex.com>
2024-08-11 20:33:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+#part 1
+my_string = input('What is the weather today? ')
+# my_string = 'Sunny and hot !'
+print('Today is',my_string)
+print("The number of chars:",len(my_string))
+
+#part 2
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[-1])
